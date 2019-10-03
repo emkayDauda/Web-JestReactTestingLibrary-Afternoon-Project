32c42f7a98343357f928328dba18512409f86946
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-03 14:09:37 +0100

    :white_check_mark: Test reset button

--- src/components/__tests__/Counter.test.js
+++ src/components/__tests__/Counter.test.js
@@ -39,10 +39,41 @@ describe('Counter component', () => {
 
   it('can decrement the count by one by clicking decrement', () => {
     // implement
+    const decButton = tools.queryByTestId('decButton');
+    rtl.fireEvent.click(decButton)
+    expect(tools.queryByText(/-1/)).toBeInTheDocument()
+    expect(tools.queryByText(/0/)).not.toBeInTheDocument()
+
+    rtl.fireEvent.click(decButton)
+    expect(tools.queryByText(/-2/)).toBeInTheDocument()
+    expect(tools.queryByText(/-1/)).not.toBeInTheDocument()
   });
 
   it('can reset the count clicking rest', () => {
     // implement
+    const decButton = tools.queryByTestId('decButton');
+    const incButton = tools.queryByTestId('incButton');
+    const resetButton = tools.queryByTestId('resetButton');
+    
+    rtl.fireEvent.click(decButton)
+    rtl.fireEvent.click(decButton)
+    rtl.fireEvent.click(decButton)
+    
+    expect(tools.queryByText(/-3/)).toBeInTheDocument()
+    rtl.fireEvent.click(resetButton)
+    expect(tools.queryByText(/0/)).toBeInTheDocument()
+    expect(tools.queryByText(/-3/)).not.toBeInTheDocument()
+
+
+
+    rtl.fireEvent.click(incButton)
+    rtl.fireEvent.click(incButton)
+    rtl.fireEvent.click(decButton)
+    rtl.fireEvent.click(incButton)
+    // tools.debug()
+    expect(tools.queryByText(/2/)).toBeInTheDocument()
+    rtl.fireEvent.click(resetButton)
+    expect(tools.queryByText(/0/)).toBeInTheDocument()
   });
 
   it('prevents the count from going over an upper limit', () => {
